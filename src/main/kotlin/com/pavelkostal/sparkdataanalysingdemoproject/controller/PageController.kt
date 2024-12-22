31b3a8fe1249d35bf06d4d8bf3f954a85fdee3d1
katzz <info@pavelkostal.com>
2024-12-22 21:23:00 +0100

    Some changes for Heroku deployment

--- src/main/kotlin/com/pavelkostal/sparkdataanalysingdemoproject/controller/PageController.kt
+++ src/main/kotlin/com/pavelkostal/sparkdataanalysingdemoproject/controller/PageController.kt
@@ -1,14 +1,17 @@
 package com.pavelkostal.sparkdataanalysingdemoproject.controller
 
 import com.pavelkostal.sparkdataanalysingdemoproject.service.DataAnalyse
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
-@Controller
 @RestController
 @RequestMapping("api/v1/analysis")
 class PageController (val dataAnalyse: DataAnalyse){
+
+    @GetMapping("/test")
+    fun homePage(): String {
+        return "All work"
+    }
 
     @GetMapping("/analyze-data")
     fun analyzeDataFromCsv(): String {
