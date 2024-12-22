2ab36cbc92deda0c566fde710fe6b134308ce673
katzz <info@pavelkostal.com>
2024-12-22 21:45:21 +0100

    Some changes for Heroku deployment

--- src/main/kotlin/com/pavelkostal/sparkdataanalysingdemoproject/controller/PageController.kt
+++ src/main/kotlin/com/pavelkostal/sparkdataanalysingdemoproject/controller/PageController.kt
@@ -3,6 +3,8 @@ package com.pavelkostal.sparkdataanalysingdemoproject.controller
 import com.pavelkostal.sparkdataanalysingdemoproject.service.DataAnalyse
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("api/v1/analysis")
@@ -10,7 +12,10 @@ class PageController (val dataAnalyse: DataAnalyse){
 
     @GetMapping("/test")
     fun homePage(): String {
-        return "All work"
+        val currentTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.nnnnnnnnn")
+        val formattedTime = currentTime.format(formatter)
+        return "Current time: $formattedTime"
     }
 
     @GetMapping("/analyze-data")
